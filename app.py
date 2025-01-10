20de8070f13c59b1f992940ed5702bbba219ec52
ajayrawati <ajayisheroof@gmail.com>
2025-01-10 13:04:00 +0530

    Api Created

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def fetch_movie_details(title):
     url = f"https://www.imdb.com/find/?q={title}"
     movieData =extract_info(url)
-    return jsonify(json.dumps(movieData,sort_keys=False,indent=4));
+    return send_file(json.dumps(movieData,sort_keys=False,indent=4));
 
 
 @app.route('/api/movie/top250',methods=['GET'])
